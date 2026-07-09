59e563ecd13f48b2459beb5ffdbbb3c47de019c9
Jlongarte <longarte.janire@gmail.com>
2026-07-09 11:02:30 +0200

    CORS añadidas

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -5,28 +5,39 @@ const connectDB = require("./src/config/db");
 const tasksRoutes = require("./src/routes/tasks.routes");
 const authRoutes = require("./src/routes/users.routes");
 const analyticsRoutes = require("./src/routes/analytics.routes");
-const boardRoutes = require('./src/routes/board.routes');
+const boardRoutes = require("./src/routes/board.routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Queda pendiente conocer las direcciones del frontal para configurar CORS de manera adecuada, por ahora se permite el acceso desde cualquier origen
-app.use(cors());
+// CORS
+app.use(
+  cors({
+    origin: "https://task-app-dmno.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  })
+);
+
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 connectDB();
 
+// Rutas
 app.use("/tasks", tasksRoutes);
 app.use("/users", authRoutes);
 app.use("/analytics", analyticsRoutes);
-app.use('/boards', boardRoutes);
+app.use("/boards", boardRoutes);
 
+// 404
 app.use((req, res, next) => {
   res.status(404).json({ message: "Route Not Found" });
 });
 
-app.use((err, req, res) => {
+// Errores
+app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Internal Server Error" });
 });
